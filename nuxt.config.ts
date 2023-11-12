be2d1f1cc3772b0275dd69abf07c16653a335a57
leonardo mazzoleni <leonardo.mazzoleni@tecnasoft.it>
2023-11-12 13:02:01 +0100

    hotfix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -40,11 +40,11 @@ export default defineNuxtConfig({
         lang: "En",
       },
       link: [
-        {
-          rel: "stylesheet",
-          href: "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0",
-          media: "screen",
-        },
+        // {
+        //   rel: "stylesheet",
+        //   href: "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0",
+        //   media: "screen",
+        // },
         {
           rel: "preconnect",
           href: "https://fonts.googleapis.com"
